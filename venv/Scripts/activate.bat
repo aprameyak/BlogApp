9314691dd9b448336458e33d38cf461cd4694ea6
Aprameya Kannan <101409453+aprameyak@users.noreply.github.com>
2025-03-31 14:54:44 -0400

    Resolved errors and added missed code

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\apram\OneDrive\Documents\GitHub\DjangoTutorial\venv
+set VIRTUAL_ENV=C:\Users\apram\OneDrive\Documents\GitHub\Django\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
